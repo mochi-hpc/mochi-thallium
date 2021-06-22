93f5a04d2430220af2b04aeca141579a0a2f56c7
Matthieu Dorier <mdorier@anl.gov>
2021-06-22 09:08:29 +0000

    added example of partial RPC deserialization

---
+++ examples/17_partial/server.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <thallium.hpp>
+
+namespace tl = thallium;
+
+/* IMPORTANT: this code will only run correctly
+ * if the "checksum" variant was disabled in
+ * Mercury
+ */
+
+int main() {
+
+    tl::engine myEngine("tcp", THALLIUM_SERVER_MODE);
+    std::cout << "Server running at address " << myEngine.self() << std::endl;
+
+    std::function<void(const tl::request&, int)> sum =
+        [](const tl::request& req, int x) {
+            std::cout << "Starting RPC" << std::endl;
+            int y;
+            std::tie(x, y) = req.get_input().as<int,int>();
+            std::cout << "x = " << x << ", y = " << y << std::endl;
+            std::cout << "Computing " << x << "+" << y << std::endl;
+            req.respond(x+y);
+        };
+
+    myEngine.define("sum", sum);
+
+    return 0;
+}
+
