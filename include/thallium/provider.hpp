3a653745f9dc0ef982ca2f99c0ae2cf1a6b3b5ab
Matthieu Dorier <mdorier@anl.gov>
2023-12-04 09:17:25 +0000

    removed default identity for provioders

--- include/thallium/provider.hpp
+++ include/thallium/provider.hpp
@@ -35,29 +35,36 @@ template <typename T> class provider {
     std::weak_ptr<detail::engine_impl>  m_engine_impl;
     margo_instance_id                   m_mid;
     uint16_t                            m_provider_id;
+    bool                                m_has_identity;
 
   public:
-    provider(const engine& e, uint16_t provider_id, const char* identity = "<unknown>")
+    provider(const engine& e, uint16_t provider_id, const char* identity = nullptr)
     : m_engine_impl(e.m_impl)
-    , m_provider_id(provider_id) {
+    , m_provider_id(provider_id)
+    , m_has_identity(identity != nullptr) {
         if(!e.m_impl) throw exception("Invalid engine");
         m_mid = e.get_margo_instance();
 #if MARGO_VERSION_NUM >= 1500
-        if(margo_provider_registered_identity(m_mid, provider_id)) {
-            throw exception{
-                "[thallium] a provider with the same ID (",
-                provider_id,
-                ") is already registered"};
+        auto registered_identity = margo_provider_registered_identity(m_mid, provider_id);
+        if(m_has_identity) {
+            if(registered_identity) {
+                throw exception{
+                    "[thallium] A (", registered_identity,
+                    ") provider with the same ID (",
+                    provider_id, ") is already registered"};
+            } else {
+                auto hret = margo_provider_register_identity(
+                        m_mid, m_provider_id, identity);
+                MARGO_ASSERT(hret, margo_provider_register_identity);
+            }
         }
-        auto hret = margo_provider_register_identity(
-            m_mid, m_provider_id, identity);
-        MARGO_ASSERT(hret, margo_provider_register_identity);
 #endif
     }
 
     virtual ~provider() {
 #if MARGO_VERSION_NUM >= 1500
-        margo_provider_deregister_identity(m_mid, m_provider_id);
+        if(m_has_identity)
+            margo_provider_deregister_identity(m_mid, m_provider_id);
 #endif
     }
 
